52414099227d66923ea4d332f7a163c09fd1aaf7
diegocgayoso <diegoc.gayoso@gmail.com>
2022-11-07 20:19:28 -0300

    Trabalhando na interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CicloComponent } from './components/ciclo/ciclo.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { ItemComponent } from './components/item/item.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CicloComponent
+    CicloComponent,
+    AddItemComponent,
+    ListaComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
