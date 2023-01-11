35c67b1b7e7d8bdf7010e8fce8d57dcb0cb2f50f
diegocgayoso <diegoc.gayoso@gmail.com>
2023-01-11 11:20:56 -0300

    Aula1.3 OnInit: carregando os dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
-
-  constructor() { }
+  listaDeCompra! : Array<Item>
+  constructor(private listaService: ListaDeCompraService) { }
+  
+  ngOnInit(): void {
+   this.listaDeCompra = this.listaService.getListaDeCompra();
+   console.log(this.listaDeCompra);
+  }
 }
