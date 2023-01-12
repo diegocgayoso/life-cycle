9aac159ef23e66aa9570f5c17850b81f059ef9f4
diegocgayoso <diegoc.gayoso@gmail.com>
2023-01-12 16:56:53 -0300

    Aula2.2- Captando nome do novo item  2.3- Adicionando novos itens à lista

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-
-  constructor() { }
+  valorItem! : string;
+  constructor(private listaService: ListaDeCompraService) { }
 
   ngOnInit(): void { }
+
+  adicionarItem(){
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo();
+  }
+
+  limparCampo(){
+    this.valorItem = '';
+  }
 }
--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -33,4 +33,20 @@ export class ListaDeCompraService {
     return this.listaDeCompra;
   }
 
+  criarItem(nomeDoItem: string){
+    const id = this.listaDeCompra.length + 1
+    const item : Item = {
+      id: id,
+      nome: nomeDoItem,
+      data: new Date().toLocaleString('pt-BR'),
+      comprado: false
+    }
+    return item
+  }
+
+  adicionarItemNaLista(nomeDoItem: string){
+    const item = this.criarItem(nomeDoItem)
+    this.listaDeCompra.push(item);
+  }
+
 }
